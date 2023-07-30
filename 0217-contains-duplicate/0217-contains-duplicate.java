34a2aa6dd1a12052e6603c2445c028a8e8b9e399
Mohammed Afthab <afthab2282@gmail.com>
2023-07-30 13:51:17 +0530

    Time: 12 ms (82.83%), Space: 54.7 MB (95.95%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
